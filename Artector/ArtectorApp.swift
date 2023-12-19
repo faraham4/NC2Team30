09d8192c7bbe284e77227e223a5279c599fd6e52
Farah Alamri <ms.falamri42@gmail.com>
2023-12-19 10:33:50 +0300

    Initial Commit

---
+++ Artector/ArtectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtectorApp.swift
+//  Artector
+//
+//  Created by Farah Alamri on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ArtectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Artector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Artector
+//
+//  Created by Farah Alamri on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
